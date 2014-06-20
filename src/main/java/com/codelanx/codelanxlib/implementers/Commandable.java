6531f375fb41b4974f853b50a96bac0d4da568da
Spencer Alderman <spencer@1rogue.com>
2014-06-19 21:38:22 -0400

    Add interfaces relating to sub-manager features

---
+++ src/main/java/com/codelanx/codelanxlib/implementers/Commandable.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2014 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.codelanxlib.implementers;
+
+import com.codelanx.codelanxlib.command.CommandHandler;
+
+/**
+ * Interface for a plugin that makes use of a {@link CommandHandler}
+ *
+ * @since 1.0.0
+ * @author 1Rogue
+ * @version 1.0.0
+ */
+public interface Commandable {
+
+    /**
+     * Gets the {@link CommandHandler} for the plugin
+     * 
+     * @since 1.0.0
+     * @version 1.0.0
+     * 
+     * @return The {@link CommandHandler} instance
+     */
+    public abstract CommandHandler getCommandHandler();
+
+}
