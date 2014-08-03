ceadcf465452a6da3abce507b1a2e31e1312dd27
Spencer Alderman <spencer@1rogue.com>
2014-08-03 16:06:35 -0400

    Start item/inventory utilities

---
+++ src/main/java/com/codelanx/codelanxlib/util/item/CustomItemBuilder.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2014 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.codelanxlib.util.item;
+
+/**
+ * Class description for {@link CustomItemBuilder}
+ *
+ * @since 1.0.0
+ * @author 1Rogue
+ * @version 1.0.0
+ */
+public class CustomItemBuilder extends ItemStackBuilder {
+
+}
---
+++ src/main/java/com/codelanx/codelanxlib/util/item/ItemStackBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2014 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2014 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.codelanxlib.util.item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ * Class description for {@link ItemStackBuilder}
+ *
+ * @since 1.0.0
+ * @author 1Rogue
+ * @version 1.0.0
+ */
+public class ItemStackBuilder {
+
+    protected static final String lorePrefix = ChatColor.RESET + "" + ChatColor.BLUE + "" + ChatColor.BOLD;
+    protected String name;
+    protected List<String> lore = new ArrayList<>();
+    protected Map<Enchantment, Integer> enchantments = new HashMap<>();
+    protected Material type;
+    protected int amount;
+
+    public ItemStackBuilder setName(String name) {
+        this.name = ChatColor.translateAlternateColorCodes('&', name);
+        return this;
+    }
+
+    public ItemStackBuilder addLore(String lore) {
+        this.lore.add(lorePrefix + ChatColor.translateAlternateColorCodes('&', lore));
+        return this;
+    }
+
+    public ItemStackBuilder addEnchantment(Enchantment ench, int level) {
+        this.enchantments.put(ench, level);
+        return this;
+    }
+
+    public ItemStackBuilder setType(Material type) {
+        this.type = type;
+        return this;
+    }
+
+    public ItemStackBuilder setAmount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public ItemStack build() {
+        if (this.type == null || this.amount < 0 || this.amount > this.type.getMaxStackSize()) {
+            throw new IllegalArgumentException("Illegal fields in " + this.getClass().getSimpleName() + "!");
+        }
+        ItemStack back = new ItemStack(this.type);
+        back.setAmount(this.amount);
+        ItemMeta meta = Bukkit.getItemFactory().getItemMeta(this.type);
+        meta.setDisplayName(this.name);
+        meta.setLore(this.lore);
+        back.setItemMeta(meta);
+        back.addEnchantments(this.enchantments);
+        return back;
+    }
+    
+}
