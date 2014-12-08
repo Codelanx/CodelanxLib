f3811efe9ced79a59ddc261c3ac9974c3cad01b3
Spencer Alderman <spencer@1rogue.com>
2014-12-08 12:42:08 -0500

    Add bukkit event for economy changes

---
+++ src/main/java/com/codelanx/codelanxlib/events/EconomyChangeEvent.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2014 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2014 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.codelanxlib.events;
+
+import static com.codelanx.codelanxlib.events.ReloadEvent.handlers;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Class description for {@link EconomyChangeEvent}
+ *
+ * @since 1.0.0
+ * @author 1Rogue
+ * @version 1.0.0
+ */
+public class EconomyChangeEvent extends Event {
+
+    protected static final HandlerList handlers = new HandlerList();
+    private final Player p;
+    private final double money;
+
+    public EconomyChangeEvent(Player p, double money) {
+        this.p = p;
+        this.money = money;
+    }
+
+    public Player getPlayer() {
+        return this.p;
+    }
+
+    public double getNewBalance() {
+        return this.money;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
