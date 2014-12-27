08ef944daaa26d2e24e1a3c2e6ea7e8725aa16a3
Spencer Alderman <spencer@1rogue.com>
2014-12-27 11:32:19 -0500

    Expand TimePoint API functionality

--- src/main/java/com/codelanx/codelanxlib/util/TimeUtil.java
+++ src/main/java/com/codelanx/codelanxlib/util/TimeUtil.java
@@ -19,14 +19,22 @@
  */
 package com.codelanx.codelanxlib.util;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Scoreboard;
 
 /**
  * Class description for {@link TimeUtil}
  *
- * @since 1.0.0
+ * @since 0.0.1
  * @author 1Rogue
- * @version 1.0.0
+ * @version 0.0.1
  */
 public class TimeUtil {
 
@@ -37,50 +45,19 @@ public class TimeUtil {
     public static String formatTime(long durationNS) {
         TimePoint point = TimeUtil.getTimePoint(durationNS);
         if (point.getNext() != null) {
-            return String.format("%d %s, %d %s", point.getTime(), TimeUtil.getProperUnitName(point.getUnit(), point.getTime()),
-                    point.getNext().getTime(), TimeUtil.getProperUnitName(point.getNext().getUnit(), point.getNext().getTime()));
+            return String.format("%d %s, %d %s", point.getTime(), point.properName(),
+                    point.getNextNonZero().getTime(), point.getNextNonZero().properName());
         } else {
             return String.format("%d %s", point.getTime(), TimeUtil.getProperUnitName(point.getUnit(), point.getTime()));
         }
-    }
-
-    private static class TimePoint {
-
-        private final long time;
-        private final TimeUnit unit;
-        private TimePoint next;
-
-        public TimePoint(long time, TimeUnit unit, TimePoint next) {
-            this.time = time;
-            this.unit = unit;
-            this.next = next;
-        }
-
-        public long getTime() {
-            return this.time;
-        }
-
-        public TimeUnit getUnit() {
-            return this.unit;
-        }
-
-        public TimePoint getNext() {
-            return this.next;
-        }
-
-        public TimePoint setNext(TimePoint next) {
-            this.next = next;
-            return this;
-        }
-
     }
 
     /**
      * Absolutely disgusting method of retrieving the largest non-zero times
      * difference in a nanosecond period. Forgive me for my sins
      *
-     * @version 1.0.0
-     * @since 1.0.0
+     * @version 0.0.1
+     * @since 0.0.1
      *
      * @param diff The difference in nanoseconds between two points
      * @return A new {@link TimePoint} representing the largest represented time
@@ -142,6 +119,82 @@ public class TimeUtil {
     private static String getProperUnitName(TimeUnit unit, long amount) {
         String proper = unit.toString().substring(0, 1) + unit.toString().substring(1, unit.toString().length()).toLowerCase();
         return amount != 1 ? proper : proper.substring(0, proper.length() - 1);
+    }
+
+    public static class TimePoint implements Comparable<TimePoint> {
+
+        private final long time;
+        private final TimeUnit unit;
+        private TimePoint next;
+
+        public TimePoint(long time, TimeUnit unit, TimePoint next) {
+            this.time = time;
+            this.unit = unit;
+            this.next = next;
+        }
+
+        public long getAmount(TimeUnit unit) {
+            TimePoint point = this.getPoint(unit);
+            return point == null ? 0 : point.getTime();
+        }
+
+        public TimePoint getPoint(TimeUnit unit) {
+            int diff = this.unit.compareTo(unit);
+            if (diff < 0) {
+                return null;
+            }
+            TimePoint back = this.next;
+            for (;diff > 1; diff--) {
+                back = back.next;
+            }
+            return back;
+        }
+
+        public long getTime() {
+            return this.time;
+        }
+
+        public TimeUnit getUnit() {
+            return this.unit;
+        }
+
+        public TimePoint getNext() {
+            return this.next;
+        }
+        
+        public TimePoint getNextNonZero() {
+            if (this.next == null || this.next.getTime() <= 0) {
+                return null;
+            }
+            TimePoint back = this;
+            while (back.next != null && back.next.getTime() <= 0) {
+                back = back.next;
+            }
+            return back.next;
+        }
+
+        public TimePoint setNext(TimePoint next) {
+            this.next = next;
+            return this;
+        }
+
+        public String properName() {
+            return TimeUtil.getProperUnitName(this.unit, this.time);
+        }
+
+        @Override
+        public int compareTo(TimePoint o) {
+            //No null checks, if o is null an NPE should be thrown
+            int curr = this.unit.compareTo(o.unit);
+            if (curr != 0) {
+                return curr;
+            }
+            curr = Long.compare(this.time, o.time);
+            if (curr == 0 && this.next != null && o.next != null) {
+                curr = this.next.compareTo(o.next);
+            }
+            return curr;
+        }
     }
 
 }
