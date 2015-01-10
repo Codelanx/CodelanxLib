77d815c3d369265f78923b0b0b4b0ff877edddaa
Spencer Alderman <spencer@1rogue.com>
2015-01-09 20:28:00 -0500

    Add InventoryUtil#getHeldItemSlot

--- src/main/java/com/codelanx/codelanxlib/util/InventoryUtil.java
+++ src/main/java/com/codelanx/codelanxlib/util/InventoryUtil.java
@@ -19,7 +19,12 @@
  */
 package com.codelanx.codelanxlib.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Class description for {@link InventoryUtil}
@@ -36,6 +41,41 @@ public class InventoryUtil {
     
     public static boolean hasClickedTop(InventoryClickEvent event) {
         return event.getRawSlot() == event.getSlot();
+    }
+
+    /**
+     * Returns the best approximation for the slot relevant to the item held
+     * in the player's hand
+     * <br><br>
+     * Note! This method will fail if there are {@link ItemStack} objects that
+     * exactly match the one in the hotbar.
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param p The player to get the item slot from
+     * @return The non-raw slot number of the item being held
+     */
+    public static int getHeldItemSlot(Player p) {
+        Map<Integer, ItemStack> quickMatch = new HashMap<>();
+        ItemStack item;
+        for (int i = 0; i < 9; i++) {
+            item = p.getInventory().getContents()[i];
+            if (item.getType() == p.getItemInHand().getType()) {
+                quickMatch.put(i, item);
+            }
+        }
+        if (quickMatch.size() == 1) {
+            return quickMatch.entrySet().iterator().next().getKey();
+        } else {
+            Optional<Integer> opt = quickMatch.entrySet().stream()
+                    .filter(i -> i.getValue().equals(p.getItemInHand()))
+                    .map(i -> i.getKey()).findFirst();
+            if (opt.isPresent()) {
+                return opt.get();
+            }
+        }
+        throw new IllegalStateException("Player hotbar item not found!");
     }
 
 }
