c7a228851e9ddebac119223409beec6883deecde
Spencer Alderman <spencer@1rogue.com>
2015-02-04 16:49:59 -0500

    Add example files

--- src/test/java/com/codelanx/codelanxlib/example/ExampleConfig.java
+++ src/test/java/com/codelanx/codelanxlib/example/ExampleConfig.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the Creative Commons BY-NC-ND license
  * long with this program. If not, see <https://creativecommons.org/licenses/>.
  */
-package com.codelanx.codelanxlib.test;
+package com.codelanx.codelanxlib.example;
 
 import com.codelanx.codelanxlib.config.Config;
 import com.codelanx.codelanxlib.data.FileDataType;
