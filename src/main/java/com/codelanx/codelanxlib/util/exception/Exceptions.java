2c90c58e26c32c70a78e62887512f472078fae41
Spencer Alderman <spencer@1rogue.com>
2015-02-10 11:34:00 -0500

    Add notNull and newInstance methods

--- src/main/java/com/codelanx/codelanxlib/util/exception/Exceptions.java
+++ src/main/java/com/codelanx/codelanxlib/util/exception/Exceptions.java
@@ -19,6 +19,11 @@
  */
 package com.codelanx.codelanxlib.util.exception;
 
+import com.codelanx.codelanxlib.util.Debugger;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+
 /**
  * A utility class for throwing conditional exceptions, similar to Apache's
  * {@link org.apache.commons.lang.Validate Validate} class.
@@ -29,36 +34,33 @@ package com.codelanx.codelanxlib.util.exception;
  */
 public final class Exceptions {
 
-    private Exceptions() {}
+    private Exceptions() {
+    }
 
     /**
      * Throws an {@link IllegalStateException} if the value of the {@code state}
      * parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
      * @param message The message to include in an {@link IllegalStateException}
      * @throws IllegalStateException if {@code state} is {@code false}
      */
     public static void illegalState(boolean state, String message) {
         if (!state) {
-            if (message != null) {
-                throw new IllegalStateException(message);
-            } else {
-                throw new IllegalStateException();
-            }
+            throw Exceptions.newException(IllegalStateException.class, message);
         }
     }
 
     /**
      * Throws an {@link IllegalStateException} if the value of the {@code state}
      * parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
      * @throws IllegalStateException if {@code state} is {@code false}
      */
@@ -69,32 +71,28 @@ public final class Exceptions {
     /**
      * Throws an {@link IllegalPluginAccessException} if the value of the
      * {@code state} parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
      * @param message The message to include in an
-     *                {@link IllegalPluginAccessException}
+     * {@link IllegalPluginAccessException}
      * @throws IllegalPluginAccessException if {@code state} is {@code false}
      */
     public static void illegalPluginAccess(boolean state, String message) {
         if (!state) {
-            if (message != null) {
-                throw new IllegalPluginAccessException(message);
-            } else {
-                throw new IllegalPluginAccessException();
-            }
+            throw Exceptions.newException(IllegalPluginAccessException.class, message);
         }
     }
 
     /**
      * Throws an {@link IllegalPluginAccessException} if the value of the
      * {@code state} parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
      * @throws IllegalPluginAccessException if {@code state} is {@code false}
      */
@@ -105,37 +103,105 @@ public final class Exceptions {
     /**
      * Throws an {@link UnsupportedOperationException} if the value of the
      * {@code state} parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
-     * @param message The message to include in an
-     *                {@link UnsupportedOperation}
+     * @param message The message to include in an {@link UnsupportedOperation}
      * @throws UnsupportedOperationException if {@code state} is {@code false}
      */
     public static void unsupportedOperation(boolean state, String message) {
         if (!state) {
-            if (message != null) {
-                throw new UnsupportedOperationException(message);
-            } else {
-                throw new UnsupportedOperationException();
-            }
+            throw Exceptions.newException(UnsupportedOperationException.class, message);
         }
     }
 
     /**
      * Throws an {@link UnsupportedOperationException} if the value of the
      * {@code state} parameter is {@code false}
-     * 
+     *
      * @since 0.1.0
      * @version 0.1.0
-     * 
+     *
      * @param state The conditional to verify
      * @throws UnsupportedOperationException if {@code state} is {@code false}
      */
     public static void unsupportedOperation(boolean state) {
         Exceptions.unsupportedOperation(state, null);
+    }
+
+    /**
+     * Provides a null check, and throws a custom {@link RuntimeException} as
+     * specified by the passed class parameter
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param <T> The exception type
+     * @param obj The object to check for {@code null}
+     * @param message The message to add to the exception if {@code obj} is null
+     * @param ex The {@link RuntimeException} class to instantiate
+     */
+    public static <T extends RuntimeException> void notNull(Object obj, String message, Class<T> ex) {
+        if (obj == null) {
+            throw Exceptions.newException(ex, message);
+        }
+    }
+
+    /**
+     * Provides a null check, and throws a custom {@link RuntimeException} as
+     * specified by the passed class parameter
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param <T> The exception type
+     * @param obj The object to check for {@code null}
+     * @param ex The {@link RuntimeException} class to instantiate
+     */
+    public static <T extends RuntimeException> void notNull(Object obj, Class<T> ex) {
+        Exceptions.notNull(obj, null, ex);
+    }
+
+    /**
+     * Dynamically constructs a new instance of a {@link RuntimeException}
+     * either via a {@link RuntimeException#RuntimeException(String)}
+     * constructor or a no-argument constructor
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param <T> The {@link RuntimeException} type
+     * @param ex The exception class to instantiate
+     * @param message The message to add, {@code null} if there is no message
+     * @return The newly constructed {@link RuntimeException}
+     */
+    private static <T extends RuntimeException> T newException(Class<T> ex, String message) {
+        if (message != null) {
+            try {
+                Constructor<T> c = ex.getConstructor(String.class);
+                c.setAccessible(true);
+                return c.newInstance(message);
+            } catch (NoSuchMethodException e) {
+                Debugger.print(Level.WARNING, String.format("Class '%s' does not have a String "
+                        + "message constructor! Using default constructor...", ex.getName()));
+            } catch (SecurityException
+                    | InstantiationException
+                    | IllegalAccessException
+                    | IllegalArgumentException
+                    | InvocationTargetException e) {
+                Debugger.error(e, "Error creating new exception instance");
+            }
+        }
+        //Now try here if the previous failed or message wasn't null
+        try {
+            return ex.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            Debugger.error(e, "Error creating new exception instance");
+        }
+        throw new IllegalArgumentException(String.format("Class '%s' does not have the "
+                + "appropriate constructors to be instantiated!", ex.getName()));
     }
 
 }
