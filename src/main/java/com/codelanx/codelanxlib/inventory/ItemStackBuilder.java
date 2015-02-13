001f2452a3eb61284204d6286a467b2562c2848b
Spencer Alderman <spencer@1rogue.com>
2015-02-13 11:08:35 -0500

    ItemStackBuilder documentation

--- src/main/java/com/codelanx/codelanxlib/inventory/ItemStackBuilder.java
+++ src/main/java/com/codelanx/codelanxlib/inventory/ItemStackBuilder.java
@@ -31,7 +31,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 /**
- * Class description for {@link ItemStackBuilder}
+ * Allows for the full creation of an {@link ItemStack} within a static context.
+ * All color codes ('&') are automatically converted
  *
  * @since 0.0.1
  * @author 1Rogue
@@ -46,31 +47,89 @@ public class ItemStackBuilder {
     protected Material type;
     protected int amount;
 
+    /**
+     * Sets the display name for the {@link ItemStack}
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @param name The display name
+     * @return This instance (chained)
+     */
     public ItemStackBuilder setName(String name) {
         this.name = ChatColor.translateAlternateColorCodes('&', name);
         return this;
     }
 
+    /**
+     * Adds lore to the {@link ItemStack}
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @param lore The lore to add
+     * @return This instance (chained)
+     */
     public ItemStackBuilder addLore(String lore) {
         this.lore.add(lorePrefix + ChatColor.translateAlternateColorCodes('&', lore));
         return this;
     }
 
+    /**
+     * Adds an enchantment to the {@link ItemStack}
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @param ench The {@link Enchantment} to add
+     * @param level The level of the enchantment
+     * @return This instance (chained)
+     */
     public ItemStackBuilder addEnchantment(Enchantment ench, int level) {
         this.enchantments.put(ench, level);
         return this;
     }
 
+    /**
+     * Sets the {@link Material} type for the {@link ItemStack}
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @param type The {@link Material} type to set
+     * @return This instance (chained)
+     */
     public ItemStackBuilder setType(Material type) {
         this.type = type;
         return this;
     }
 
+    /**
+     * Sets the amount of items in the {@link ItemStack}
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @param amount The amount to set
+     * @return This instance (chained)
+     */
     public ItemStackBuilder setAmount(int amount) {
         this.amount = amount;
         return this;
     }
 
+    /**
+     * Builds the {@link ItemStack} object and returns it
+     * 
+     * @since 0.0.1
+     * @version 0.0.1
+     * 
+     * @return The new {@link ItemStack}
+     * @throws IllegalArgumentException If type is {@code null},
+     *                                  {@code amount < 0}, or
+     *                                  {@code amount > }
+     *                                  {@link Material#getMaxStackSize}
+     */
     public ItemStack build() {
         if (this.type == null || this.amount < 0 || this.amount > this.type.getMaxStackSize()) {
             throw new IllegalArgumentException("Illegal fields in " + this.getClass().getSimpleName() + "!");
