ba8639481402d56f03540f8b437ce171adbacc21
Spencer Alderman <spencer@1rogue.com>
2015-02-14 11:10:02 -0500

    MenuIcon documentation

--- src/main/java/com/codelanx/codelanxlib/inventory/iinterface/MenuIcon.java
+++ src/main/java/com/codelanx/codelanxlib/inventory/iinterface/MenuIcon.java
@@ -20,13 +20,13 @@
 package com.codelanx.codelanxlib.inventory.iinterface;
 
 import com.codelanx.codelanxlib.config.Config;
+import com.codelanx.codelanxlib.util.RNG;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.bukkit.entity.Player;
@@ -36,43 +36,77 @@ import org.bukkit.inventory.ItemStack;
  * Represents an {@link ItemStack} in an {@link InventoryPanel}'s inventory that
  * will execute code when clicked.
  *
- * @since 0.0.1
+ * @since 0.1.0
  * @author 1Rogue
- * @version 0.0.1
+ * @version 0.1.0
  */
 public class MenuIcon {
 
-    protected static final Random RAND = new Random();
+    /** A mapping of metadata options */
     protected Map<String, Object> options;
+    /** A seed representing this {@link MenuIcon object} */
     protected final long seed;
+    /** The {@link ItemStack} that this instance uses */
     protected final ItemStack item;
+    /** The {@link Execution} function to run when clicked */
     protected Execution onExec;
+    /** Permissions required to see this icon */
     protected final List<String> perms = new ArrayList<>();
 
     /**
-     * 
-     * @param item
-     * @param onExec
-     * @param options 
+     * Initializes fields
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param item The {@link ItemStack} to use for display in the inventory
+     * @param onExec The {@link Execution} to use when clicked
+     * @param options A mapping of metadata options
      */
     MenuIcon(ItemStack item, Execution onExec, Map<String, Object> options) {
         if (item == null || options == null) {
             throw new IllegalArgumentException("Constructor parameters cannot be null!");
         }
         this.item = item;
-        this.seed = MenuIcon.RAND.nextLong();
+        this.seed = RNG.THREAD_LOCAL().nextLong();
         this.onExec = onExec;
         this.options = options;
     }
 
+    /**
+     * Returns the {@link ItemStack} that is displayed when a user opens the
+     * {@link InventoryInterface}
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @return The {@link ItemStack} to display
+     */
     public ItemStack getItem() {
         return this.item;
     }
 
+    /**
+     * Sets the {@link Executable} function that is called when this icon is
+     * clicked. Can be set to {@code null} to not execute anything
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param onExec The {@link Executable} to be used
+     */
     public void setExecutable(Execution onExec) {
         this.onExec = onExec;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @return {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -81,18 +115,55 @@ public class MenuIcon {
         return hash;
     }
 
+    /**
+     * Returns a {@link Map} of all the metadata options
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @return A {@link Map} of metadata values
+     */
     public Map<String, Object> getOptions() {
         return Collections.unmodifiableMap(this.options);
     }
 
+    /**
+     * Adds a metadata option
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param key The {@link String} key to retrieve the option
+     * @param value The value of the option
+     * @return The previous value associated with the key, or {@code null} if
+     * there was no previous mapping using the key
+     */
     public Object addOption(String key, Object value) {
         return this.options.put(key, value);
     }
 
+    /**
+     * Returns a metadata option
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param key The key that maps to the value
+     * @return The stored metadata value, or {@code null} if none is set
+     */
     public Object getOption(String key) {
         return this.options.get(key);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param obj {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -108,12 +179,34 @@ public class MenuIcon {
         return Objects.equals(this.item, other.item);
     }
 
+    /**
+     * Runs the {@link Executable} associated with this icon if one is set
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param p The {@link Player} that clicked the icon
+     * @param ii The {@link InventoryInterface} associated with this icon
+     */
     void execute(Player p, InventoryInterface ii) {
         if (this.onExec != null) {
             this.onExec.onExec(p, ii, this);
         }
     }
 
+    /**
+     * Manually deserializes a YAML mapping and returns the appropriate
+     * {@link MenuIcon}. This method is permitted to fail, if values are missing
+     * or malformed from the yaml file, it will throw an exception (Most like a
+     * {@link NullPointerException}
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param ii The {@link InventoryInterface} to serialize for
+     * @param o The YAML mapping to deserialize
+     * @return A new {@link MenuIcon} instance
+     */
     static MenuIcon valueOf(InventoryInterface ii, Object o) {
         Map<String, Object> map = Config.getConfigSectionValue(o);
         if (map == null || map.isEmpty()) {
@@ -129,7 +222,7 @@ public class MenuIcon {
                 back.perms.addAll(perm);
             }
             if (link != null) {
-                new Timer().schedule(new TimerTask() {          
+                new Timer().schedule(new TimerTask() {
                     @Override
                     public void run() {
                         InventoryPanel ip = ii.find(p -> link.equalsIgnoreCase(p.getSerializedName()));
@@ -145,6 +238,15 @@ public class MenuIcon {
         }
     }
 
+    /**
+     * Converts the current instance into a {@link Map} for serialization
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param ii The relevant {@link InventoryInterface} for this serialization
+     * @return A mapping of this object's values
+     */
     Map<String, Object> toMap(InventoryInterface ii) {
         Map<String, Object> back = new HashMap<>();
         back.put("item", this.item);
@@ -158,14 +260,41 @@ public class MenuIcon {
         return back;
     }
 
+    /**
+     * Adds a permission node that a player will need in order to see this icon.
+     * If no permissions are set, everyone is capable of seeing the icon. If
+     * multiple permissions are set, only one of the permission nodes is needed
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param permission A string representing a permission node
+     */
     public void addPermission(String permission) {
         this.perms.add(permission);
     }
 
+    /**
+     * Returns {@code true} if any permissions are required to see this icon
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @return {@code true} if a permission is needed to see the icon
+     */
     public boolean requiresPerms() {
         return !this.perms.isEmpty();
     }
 
+    /**
+     * Returns whether or not a {@link Player} has permission to view this icon
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param p The {@link Player} to check
+     * @return {@code true} if the player has permission to view the icon
+     */
     public boolean hasPermission(Player p) {
         return this.requiresPerms() ? this.perms.stream().anyMatch(p::hasPermission) : true;
     }
