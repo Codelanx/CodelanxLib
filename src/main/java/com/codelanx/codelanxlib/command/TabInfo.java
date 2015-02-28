bf65b938d1afe236c61bafce00c27f7a77713d96
Spencer Alderman <spencer@1rogue.com>
2015-02-27 20:13:05 -0500

    Add BiFunction to TabInfo to support CommandSender checking

--- src/main/java/com/codelanx/codelanxlib/command/TabInfo.java
+++ src/main/java/com/codelanx/codelanxlib/command/TabInfo.java
@@ -25,9 +25,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import org.apache.commons.lang.Validate;
+import org.bukkit.command.CommandSender;
 
 /**
  * Utility class for mapping command arguments to simplify basic uses of
@@ -82,7 +84,7 @@ public class TabInfo {
      * @version 0.1.0
      * 
      * @param argCount The number of arguments for these defaults
-     * @param defaults The defaults to set
+     * @param defaults A {@link Supplier} that returns a {@link List} of strings
      */
     public void delegate(int argCount, Supplier<? extends List<String>> defaults) {
         Validate.notNull(defaults);
@@ -94,12 +96,55 @@ public class TabInfo {
      * argument being parsed, or {@code null} if there is no argument to parse,
      * and returns a {@link List List&lt;String&gt;} of the results
      * 
-     * @param argCount
-     * @param defaults 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param argCount The number of arguments for these defaults
+     * @param defaults A {@link Function} which accepts a possibly null argument
+     *                 that is being parsed, and returns a {@link List} of
+     *                 possible strings for arguments
      */
     public void act(int argCount, Function<String, ? extends List<String>> defaults) {
         Validate.notNull(defaults);
         this.defaults.put(argCount, new SupplierContainer(defaults));
+    }
+
+    /**
+     * Maps an argument count to a {@link BiFunction} that accepts the
+     * {@link CommandSender} who tabbed, the relevant argument being parsed, or
+     * {@code null} if there is no argument to parse, and returns a
+     * {@link List List&lt;String&gt;} of the results.
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     * 
+     * @param argCount The number of arguments for these defaults
+     * @param defaults A {@link BiFunction} that accepts the sender who tabbed,
+     *                 the relevant argument being tabbed (or null), and returns
+     *                 a {@link List} of possible string arguments to fill with
+     */
+    public void act(int argCount, BiFunction<CommandSender, String, ? extends List<String>> defaults) {
+        Validate.notNull(defaults);
+        this.defaults.put(argCount, new SupplierContainer(defaults));
+    }
+
+    /**
+     * Returns {@link TabInfo#apply(CommandSender, String...) this.apply(null, args)}
+     * <br><br>
+     * Applies this {@link TabInfo} to a series of command arguments. If the
+     * final argument is incomplete, then any default arguments which do not
+     * start with the specified incomplete argument will be removed from the
+     * possible results
+     *
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @see TabInfo#apply(CommandSender, String...)
+     * @param args The arguments being tested
+     * @return The possible arguments that could be used
+     */
+    public List<String> apply(String... args) {
+        return this.apply(null, args);
     }
 
     /**
@@ -111,10 +156,11 @@ public class TabInfo {
      * @since 0.1.0
      * @version 0.1.0
      *
+     * @param sender The {@link CommandSender} who is tabbing
      * @param args The arguments being tested
      * @return The possible arguments that could be used
      */
-    public List<String> apply(String... args) {
+    public List<String> apply(CommandSender sender, String... args) {
         String arg = null;
         int get = args.length - 1;
         if (args.length > 0) {
@@ -126,7 +172,7 @@ public class TabInfo {
         if (back == null) {
             return CommandNode.BLANK_TAB_COMPLETE;
         } else {
-            return back.apply(arg);
+            return back.apply(sender, arg);
         }
     }
 
@@ -136,27 +182,38 @@ public class TabInfo {
         private final List<String> list;
         private final Supplier<? extends List<String>> supplier;
         private final Function<String, ? extends List<String>> function;
+        private final BiFunction<CommandSender, String, ? extends List<String>> biFunction;
         
         public SupplierContainer(List<String> list) {
             this.list = Collections.unmodifiableList(list);
             this.supplier = null;
             this.function = null;
+            this.biFunction = null;
         }
         
         public SupplierContainer(Supplier<? extends List<String>> supplier) {
             this.list = null;
             this.supplier = supplier;
             this.function = null;
+            this.biFunction = null;
         }
         
         public SupplierContainer(Function<String, ? extends List<String>> function) {
             this.list = null;
             this.supplier = null;
             this.function = function;
+            this.biFunction = null;
+        }
+        
+        public SupplierContainer(BiFunction<CommandSender, String, ? extends List<String>> biFunction) {
+            this.list = null;
+            this.supplier = null;
+            this.function = null;
+            this.biFunction = biFunction;
         }
 
         //While a bit messy, applies logic for each type
-        public List<String> apply(String arg) {
+        public List<String> apply(CommandSender sender, String arg) {
             if (this.list != null) {
                 if (arg == null) {
                     return this.list; //already unmodifiable
@@ -169,6 +226,8 @@ public class TabInfo {
                 return this.supplier.get();
             } else if (this.function != null) {
                 return this.function.apply(arg);
+            } else if (this.biFunction != null) {
+                return this.biFunction.apply(sender, arg);
             }
             throw new IllegalStateException("All container fields are null");
         }
