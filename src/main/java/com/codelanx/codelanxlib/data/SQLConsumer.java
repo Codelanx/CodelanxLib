0a38678380f242004dd3e3379a8788fc9731b752
Spencer Alderman <spencer@1rogue.com>
2015-03-15 10:01:14 -0400

    Add SQLConsumer

---
+++ src/main/java/com/codelanx/codelanxlib/data/SQLConsumer.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 Codelanx, All Rights Reserved
+ *
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * This program is protected software: You are free to distrubute your
+ * own use of this software under the terms of the Creative Commons BY-NC-ND
+ * license as published by Creative Commons in the year 2015 or as published
+ * by a later date. You may not provide the source files or provide a means
+ * of running the software outside of those licensed to use it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the Creative Commons BY-NC-ND license
+ * long with this program. If not, see <https://creativecommons.org/licenses/>.
+ */
+package com.codelanx.codelanxlib.data;
+
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * Represents an operation that accepts a single input argument and returns no
+ * result. Unlike most other functional interfaces, {@code Consumer} is expected
+ * to operate via side-effects.
+ * 
+ * @since 0.1.0
+ * @author 1Rogue
+ * @version 0.1.0
+ * 
+ * @see Consumer
+ * @param <T> The type of the input to the operation
+ */
+@FunctionalInterface
+public interface SQLConsumer<T> {
+
+    /**
+     * Performs this operation on the given argument.
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     * @param t the input argument
+     * @throws SQLException If an {@link SQLException} is thrown in the lambda
+     *                      body
+     */
+    void accept(T t) throws SQLException;
+
+    /**
+     * Returns a composed {@code Consumer} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     * 
+     * @since 0.1.0
+     * @version 0.1.0
+     *
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code Consumer} that performs in sequence this
+     * operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default SQLConsumer<T> andThen(Consumer<? super T> after) {
+        Objects.requireNonNull(after);
+        return (T t) -> { accept(t); after.accept(t); };
+    }
+
+}
